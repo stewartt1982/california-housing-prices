c1be59a02c4a1fef01c275295f38af1b8d53f02f
stewartt1982 <stewartt1982@gmail.com>
2018-05-16 18:57:39 -0300

    Rationalised the code a bit, moving each step into a function to leave the main function more clear

--- california_housing_prices.py
+++ california_housing_prices.py
@@ -119,15 +119,126 @@ def load_data_csv(path="",file=""):
 def print_gridsearch_cv(gridsearch,name="estimator"):
     print "Best Results: ",name,gridsearch.cv_results_['params'][gridsearch.best_index_]
     print "Best Score: ",name, np.sqrt(-gridsearch.best_score_)
-    
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = "datasets/housing"
-HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
 
-#fetch the data from the remote url
-fetch_data(HOUSING_URL,"input")
-#load the data into a dataframe
-data_all = load_data_csv("input","housing.csv")
+def create_train_test(data=None,opt=2,split_size=0.2):
+    #default opt=1 setting uses train_test_split
+    #if opt=2 use tatified sampling
+    data["ocean_proximity"]=data["ocean_proximity"].astype('category')
+    if opt==1:
+        train_set, test_set = train_test_split(data,test_size=split_size,random_state=13)
+    elif opt==2:
+        data["income_cat"] = np.ceil(data["median_income"] / 1.5)
+        data["income_cat"].where(data["income_cat"] < 5, 5.0, inplace=True)
+        split = StratifiedShuffleSplit(n_splits=1,test_size=split_size,random_state=13)
+        for train_index_strat, test_index_strat in split.split(data, data["income_cat"]):
+            train_set = data.loc[train_index_strat]
+            test_set = data.loc[test_index_strat]
+        #drop the income_cat variable used by StratifiedShuffleSplit
+        for set in (train_set, test_set):
+            set.drop(["income_cat"], axis=1, inplace=True) 
+
+    return train_set, test_set
+
+def get_target_set(train_set,test_set):
+    #get set without the target
+    train_set_notarget = train_set.drop("median_house_value",axis=1)
+    test_set_notarget = test_set.drop("median_house_value",axis=1)
+    #save the target
+    train_target = train_set["median_house_value"].copy()
+    test_target = test_set["median_house_value"].copy()
+    
+    return train_set_notarget,test_set_notarget,train_target,test_target
+
+
+def create_pipeline():
+    #Select features which are numerical and impute missing values by median
+    #columns for attributes
+    total_rooms = train_set.columns.get_loc("total_rooms")
+    households  = train_set.columns.get_loc("households")
+    total_bedrooms = train_set.columns.get_loc("total_bedrooms")
+    population = train_set.columns.get_loc("population")
+    
+    NumPipeline = Pipeline([
+        ('numerical',TypeSelector(np.number)),
+        ('imputer',Imputer(strategy='median')),
+        ('attrib_divider1',AttributeDivider(total_rooms,households)),
+        ('attrib_divider2',AttributeDivider(total_bedrooms,total_rooms)),
+        ('attrib_divider3',AttributeDivider(population,households)),
+        ('std_scaler',StandardScaler())
+    ])
+    
+    
+    
+    #pipeline 2.
+    #Select categorical features and convert them to onehotencodered numberical
+    CatPipeline = Pipeline([
+        ('categoricals',TypeSelector('category')),
+        ('stringindexer',StringIndexer()),
+        ('cat_encoder', OneHotEncoder(sparse=False))
+    ])
+
+
+
+    FullPipeline = FeatureUnion(transformer_list=[
+        ("num_pipeline", NumPipeline),
+        ("cat_pipeline", CatPipeline)
+    ]) 
+    return FullPipeline
+
+def find_best_model_param(data=None,target=None):
+    pipe_LR = Pipeline([
+        ('reg', LinearRegression())
+    ])
+    param_grid_LR = [
+        {
+            'reg': [LinearRegression()]
+        }
+    ]
+    param_grid_DT = [
+        {
+            'reg': [DecisionTreeRegressor()],
+            "reg__min_samples_split": [2, 10, 20],
+            "reg__max_depth": [None, 2, 5, 10],
+            "reg__min_samples_leaf": [1, 5, 10],
+            "reg__max_leaf_nodes": [None, 5, 10, 20],
+        }
+    ]
+    param_grid_RF = [
+        {
+            'reg': [RandomForestRegressor()],
+            'reg__bootstrap': [True, False],
+            'reg__n_estimators': [3, 10, 30],
+            'reg__max_features': [2, 4, 6, 8]
+        }
+    ]
+    
+    grid_LR = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_LR,scoring='neg_mean_squared_error')
+    grid_LR.fit(data,target)
+    grid_DT = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_DT,scoring='neg_mean_squared_error')
+    grid_DT.fit(data,target)
+    grid_RF = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_RF,scoring='neg_mean_squared_error')
+    grid_RF.fit(data,target)
+
+    print_gridsearch_cv(grid_LR)
+    print_gridsearch_cv(grid_DT)
+    print_gridsearch_cv(grid_RF)
+
+    return grid_RF.best_estimator_
+
+
+if __name__=='__main__':
+    #Specify download path and location where data is stored
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+    HOUSING_PATH = "datasets/housing"
+    HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+    
+    #fetch the data from the remote url
+    fetch_data(HOUSING_URL,"input")
+
+    #load the data into a dataframe
+    data_all = load_data_csv("input","housing.csv")
+
+    #Create training set and test set
 
 #We will now setup the pipeline to deal with the daa and
 #prepare it for use
@@ -136,61 +247,16 @@ data_all = load_data_csv("input","housing.csv")
 #Deal with the categoical variable ocean_proximity by encodeing
 #Impute missing values (if any)
 #Scale data
-#Aplly function, such as log, to data with long tails
+#Apply function, such as log, to data with long tails
 
-data_all["ocean_proximity"]=data_all["ocean_proximity"].astype('category')
-data_all["income_cat"] = np.ceil(data_all["median_income"] / 1.5)
-data_all["income_cat"].where(data_all["income_cat"] < 5, 5.0, inplace=True)
-split = StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=13)
-for train_index_strat2, test_index_strat2 in split.split(data_all, data_all["income_cat"]):
-    train_set_strat2 = data_all.loc[train_index_strat2]
-    test_set_strat2 = data_all.loc[test_index_strat2]
-for set in (train_set_strat2, test_set_strat2):
-    set.drop(["income_cat"], axis=1, inplace=True) 
+train_set_strat2,test_set_strat2 = create_train_test(data=data_all,opt=2,split_size=0.2)
 
-#drop the target
-train_set = train_set_strat2.drop("median_house_value",axis=1)
-test_set = test_set_strat2.drop("median_house_value",axis=1)
-#save the target
-train_target = train_set_strat2["median_house_value"].copy()
-test_target = test_set_strat2["median_house_value"].copy()
+#drop the target and save target
+train_set,test_set,train_target,test_target = get_target_set(train_set_strat2, test_set_strat2)
 
-#pipeline 1.
-#Select features which are numerical and impute missing values by median
-#columns for attributes
-total_rooms = train_set.columns.get_loc("total_rooms")
-households  = train_set.columns.get_loc("households")
-total_bedrooms = train_set.columns.get_loc("total_bedrooms")
-population = train_set.columns.get_loc("population")
-
-NumPipeline = Pipeline([
-    ('numerical',TypeSelector(np.number)),
-    ('imputer',Imputer(strategy='median')),
-    ('attrib_divider1',AttributeDivider(total_rooms,households)),
-    ('attrib_divider2',AttributeDivider(total_bedrooms,total_rooms)),
-    ('attrib_divider3',AttributeDivider(population,households)),
-#    ('std_scaler',StandardScaler())
-])
-
-
-
-#pipeline 2.
-#Select categorical features and convert them to onehotencodered numberical
-CatPipeline = Pipeline([
-    ('categoricals',TypeSelector('category')),
-    ('stringindexer',StringIndexer()),
-    ('cat_encoder', OneHotEncoder(sparse=False))
-])
-
-
-
-FullPipeline = FeatureUnion(transformer_list=[
-    ("num_pipeline", NumPipeline),
-    ("cat_pipeline", CatPipeline)
-]) 
-
+FullPipeline = create_pipeline()
 full_output = FullPipeline.fit_transform(train_set)
-
+test_output = FullPipeline.transform(test_set)
 
 #Now that we have the full provessed data we want to train a number of different models
 #using either GridSearchCV or RandomSearchCV to optimise the hyper-parameters
@@ -205,96 +271,9 @@ full_output = FullPipeline.fit_transform(train_set)
 #GradientBoostingTree
 #create pipelines
 #LinearRegression
-pipe_LR = Pipeline([
-    ('reg', LinearRegression())
-])
-param_grid_LR = [
-    {
-        'reg': [LinearRegression()]
-    }
-]
-param_grid_DT = [
-    {
-        'reg': [DecisionTreeRegressor()],
-        "reg__min_samples_split": [2, 10, 20],
-        "reg__max_depth": [None, 2, 5, 10],
-        "reg__min_samples_leaf": [1, 5, 10],
-        "reg__max_leaf_nodes": [None, 5, 10, 20],
-    }
-]
-param_grid_RF = [
-    {
-        'reg': [RandomForestRegressor()],
-        'reg__bootstrap': [True, False],
-        'reg__n_estimators': [3, 10, 30],
-        'reg__max_features': [2, 4, 6, 8]
-     }
-]
 
-grid_LR = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_LR,scoring='neg_mean_squared_error')
-grid_LR.fit(full_output,train_target)
-grid_DT = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_DT,scoring='neg_mean_squared_error')
-grid_DT.fit(full_output,train_target)
-grid_RF = GridSearchCV(pipe_LR, cv=5, n_jobs=1, param_grid=param_grid_RF,scoring='neg_mean_squared_error')
-grid_RF.fit(full_output,train_target)
-
-print_gridsearch_cv(grid_LR)
-print_gridsearch_cv(grid_DT)
-print_gridsearch_cv(grid_RF)
-
-#Best model is the RandomForest
-#predict the values from the test set and check the RMSE
-test_output = FullPipeline.transform(test_set)
-
-final_model = grid_RF.best_estimator_
-test_predictions = grid_RF.best_estimator_.predict(test_output)
+final_model = find_best_model_param(full_output,train_target)
+test_predictions = final_model.predict(test_output)
 test_mse = mean_squared_error(test_predictions,test_target)
 test_rmse = np.sqrt(test_mse)
 print "Final RMSE: ",test_rmse
-
-# #Lets start off by training a linear regression model
-# linreg = LinearRegression()
-# linreg.fit(full_output, train_target)
-# predictions = linreg.predict(full_output)
-# linmse = mean_squared_error(train_target, predictions) 
-# linrmse = np.sqrt(linmse)
-# print linrmse
-# dectreereg =  DecisionTreeRegressor()
-# dectreereg.fit(full_output, train_target)
-# predictions2 = dectreereg.predict(full_output)
-# dectreemse = mean_squared_error(train_target, predictions2) 
-# dectreermse = np.sqrt(dectreemse)
-# print dectreermse
-
-
-# #cross validation for the two models
-# linscores = cross_val_score(linreg,full_output,train_target,scoring="neg_mean_squared_error",cv=10)
-# linrmse_scores = np.sqrt(-linscores)
-
-# print linrmse_scores
-# print linrmse_scores.mean()
-# print linrmse_scores.std()
-
-
-# dectreescores = cross_val_score(dectreereg,full_output,train_target,scoring="neg_mean_squared_error",cv=10)
-# dectreermse_scores = np.sqrt(-dectreescores)
-
-# print dectreermse_scores
-# print dectreermse_scores.mean()
-# print dectreermse_scores.std()
-
-
-# #let us try to not overfit as much and try a RandomForest regression
-# rndforestreg =  RandomForestRegressor()
-# rndforestreg.fit(full_output, train_target)
-# predictions3 = rndforestreg.predict(full_output)
-# rndforestmse = mean_squared_error(train_target, predictions3)
-# rndforestrmse = np.sqrt(rndforestmse)
-# print rndforestrmse
-
-# rndforestscores = cross_val_score(rndforestreg,full_output,train_target,scoring="neg_mean_squared_error",cv=10)
-# rndforestrmse_scores = np.sqrt(-rndforestscores)
-
-# print rndforestrmse_scores
-# print rndforestrmse_scores.mean()
-# print rndforestrmse_scores.std()
